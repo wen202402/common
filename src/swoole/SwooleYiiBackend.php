2fe4ad4cdf66bfc144fefc2cc7f92d2bc8a43498
Your Name <you@example.com>
2026-07-26 17:30:09 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -7,7 +7,7 @@ use wen202402\common\helper\FileHelper;
 use Yii;
 use yii\base\BaseObject;
 use yii\base\InvalidConfigException;
-
+\Swoole\Runtime::enableCoroutine();
 class SwooleYiiBackend extends BaseObject{
     public $host     = '0.0.0.0';
     public $port     = 58000;
