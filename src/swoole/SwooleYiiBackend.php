29db8c0e09c5238e56c7f724693ff1d4aff72181
Your Name <you@example.com>
2026-07-27 08:48:31 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -87,7 +87,7 @@ class SwooleYiiBackend extends BaseObject{
         FileHelper::chmod755($app['aliases']['@app'] . DIRECTORY_SEPARATOR . 'modules');
         FileHelper::chmod755($app['aliases']['@app'] . DIRECTORY_SEPARATOR . 'runtime');
         FileHelper::chmod755($app['aliases']['@console'] . DIRECTORY_SEPARATOR . 'runtime');
-        $this->getIP();
+       // $this->getIP();
         printf("listen on http://%s:%d\n", $server->host, $server->port);
 
 
