432d3af6bae683ef639e9c22519c96fcfa467f04
Your Name <you@example.com>
2026-07-29 16:12:27 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -20,6 +20,7 @@ class SwooleYiiBackend extends BaseObject{
     public $mode     = SWOOLE_PROCESS;
     public $sockType = SWOOLE_SOCK_TCP;
     public $document_root ;
+    public $cpu =0.6;
     public $options = [
         'pid_file' =>  'backend/runtime/swoole.pid',
         'log_file' =>    'backend/runtime/swoole.log',
@@ -51,7 +52,7 @@ class SwooleYiiBackend extends BaseObject{
 
         $this->options['pid_file']=  ($docroot = $this->document_root . DIRECTORY_SEPARATOR) . 'backend/runtime/swoole.pid';
         $this->options['log_file']= $docroot . 'backend/runtime/swoole.log';
-        $this->options['worker_num']=   (int)(swoole_cpu_num() * 0.6) ?: 2;                                                          //建议：部署后用监控工具（如 Prometheus + Grafana）观测数据库连接数和 Redis 内存占用，再根据实际情况调整 Worker 数量。
+        $this->options['worker_num']=   (int)(swoole_cpu_num() *$this->cpu) ?: 2;                                                          //建议：部署后用监控工具（如 Prometheus + Grafana）观测数据库连接数和 Redis 内存占用，再根据实际情况调整 Worker 数量。
         $this->options['document_root']= $docroot . 'backend' . DIRECTORY_SEPARATOR . 'web';
 
 
