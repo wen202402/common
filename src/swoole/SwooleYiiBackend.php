3244d07970a852a9f71603e5833ca92dd6a55451
Your Name <you@example.com>
2026-07-29 11:58:00 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -30,7 +30,7 @@ class SwooleYiiBackend extends BaseObject{
         'enable_coroutine' => true,
         'open_mqtt_protocol' => false,
         'open_tcp_nodelay' => true,
-        'max_request' => 10000,                                                                                               //偶然重启释放点内存
+        'max_request' => 20000,                                                                                               //偶然重启释放点内存
         'dispatch_mode' => 2,                                                                                               //模式1（固定/负载相关的分发） 模式2（轮询/均衡分发） 模式 3（自定义/更严格的映射分发，取决于版本）
         'log_level' => SWOOLE_LOG_WARNING,
         'user' => 'www',
