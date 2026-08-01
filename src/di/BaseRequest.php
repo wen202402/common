688abe578779e92cd48325889a538fa88e6ba3db
Your Name <you@example.com>
2026-08-01 08:54:48 +0800

    dev-main

--- src/di/BaseRequest.php
+++ src/di/BaseRequest.php
@@ -33,6 +33,7 @@ class BaseRequest  extends \yii\web\Request{
         $this->_request = $request;
         $this->setupHeaders();
         $this->setupGlobalVars($request,$document_root);
+        Yii::$app->response->clear();
     }
 
 
@@ -60,57 +61,52 @@ class BaseRequest  extends \yii\web\Request{
 
         if ($get === [] && $queryString !== '') parse_str($queryString, $get);
         if ($queryString === '' && $get !== []) $queryString = http_build_query($get);
-
-        $requestPath = (string)($server['request_uri'] ?? '/');
-        $requestPath = parse_url($requestPath, PHP_URL_PATH) ?: '/';
-        $requestUri = $requestPath . ($queryString !== '' ? '?' . $queryString : '');
-
-        $scriptFilename = ($documentRoot = rtrim((string)$document_root, DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR . 'index.php';
-
-        $_GET = $get;
-        $_POST = $post;
-        $_FILES = $files;
-        $_COOKIE = $cookies;
-        $_SERVER = [];
-
+        $requestPath                                = (string)($server['request_uri'] ?? '/');
+        $requestPath                                = parse_url($requestPath, PHP_URL_PATH) ?: '/';
+        $requestUri                                 = $requestPath . ($queryString !== '' ? '?' . $queryString : '');
+        $scriptFilename                              = ($documentRoot = rtrim((string)$document_root, DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR . 'index.php';
+        $_GET                                       = $get;
+        $_POST                                      = $post;
+        $_FILES                                     = $files;
+        $_COOKIE                                    = $cookies;
+        $_SERVER                                    = [];
         foreach ($server as $name => $value) $_SERVER[strtoupper($name)] = $value;
-
         foreach ($headers as $name => $value) {
             $serverName = strtoupper(str_replace('-', '_', $name));
-
             if ($serverName === 'CONTENT_TYPE' || $serverName === 'CONTENT_LENGTH') {
                 $_SERVER[$serverName] = $value;
                 continue;
             }
-
             $_SERVER['HTTP_' . $serverName] = $value;
         }
 
-        $host = (string)($headers['host'] ?? $server['server_name'] ?? $this->host);
-        $forwardedProto = strtolower((string)($headers['x-forwarded-proto'] ?? ''));
-        $https = $forwardedProto === 'https' || (int)($server['server_port'] ?? 0) === 443;
-
-        $_SERVER['REQUEST_METHOD'] = strtoupper((string)($server['request_method'] ?? 'GET'));
-        $_SERVER['REQUEST_URI'] = $requestUri;
-        $_SERVER['QUERY_STRING'] = $queryString;
-        $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $_SERVER['PHP_SELF'] = '/index.php';
-        $_SERVER['SCRIPT_FILENAME'] = $scriptFilename;
-        $_SERVER['DOCUMENT_ROOT'] = $documentRoot;
-        $_SERVER['HTTP_HOST'] = $host;
-        $_SERVER['SERVER_NAME'] = preg_replace('/:\d+$/', '', $host);
-        $_SERVER['SERVER_PORT'] = (string)($server['server_port'] ?? ($https ? 443 : 80));
-        $_SERVER['SERVER_PROTOCOL'] = (string)($server['server_protocol'] ?? 'HTTP/1.1');
-        $_SERVER['REMOTE_ADDR'] = (string)($server['remote_addr'] ?? '');
-        $_SERVER['REMOTE_PORT'] = (string)($server['remote_port'] ?? '');
-        $_SERVER['REQUEST_SCHEME'] = $https ? 'https' : 'http';
-        $_SERVER['HTTPS'] = $https ? 'on' : 'off';
-
-        //    unset($_SERVER['PATH_INFO']);
+        $host                                     = (string)($headers['host'] ?? $server['server_name'] ?? $this->host);
+        $host                                     = preg_replace('/:\d+$/', '', $host) ?: $host;
+        $forwardedProto                           = strtolower(trim((string)($headers['x-forwarded-proto'] ?? $headers['X-Forwarded-Proto'] ?? '')));
+        $xfSSL                                    = strtolower(trim((string)($headers['x-forwarded-ssl'] ?? $headers['X-Forwarded-Ssl'] ?? '')));
+        $port                                     = (int)($server['server_port'] ?? 0);
+        $https = false;
+        if ($forwardedProto === 'https')        $https = true;
+        elseif ($port === 443)                  $https = true;
+        elseif ($xfSSL === 'on')                $https = true;
+        $_SERVER['REQUEST_SCHEME']               = $https ? 'https' : 'http';
+        $_SERVER['HTTPS']                        = $https ? 'on' : 'off';
+        $_SERVER['SERVER_PORT']                  = (string)($server['server_port'] ?? ($https ? 443 : 80));
+        $_SERVER['REQUEST_METHOD']               = strtoupper((string)($server['request_method'] ?? 'GET'));
+        $_SERVER['REQUEST_URI']                  = $requestUri;
+        $_SERVER['QUERY_STRING']                 = $queryString;
+        $_SERVER['SCRIPT_NAME']                  = '/index.php';
+        $_SERVER['PHP_SELF']                     = '/index.php';
+        $_SERVER['SCRIPT_FILENAME']              = $scriptFilename;
+        $_SERVER['DOCUMENT_ROOT']                 = $documentRoot;
+        $_SERVER['HTTP_HOST']                    = $host;
+        $_SERVER['SERVER_NAME']                  = preg_replace('/:\d+$/', '', $host);
+        $_SERVER['SERVER_PROTOCOL']              = (string)($server['server_protocol'] ?? 'HTTP/1.1');
+        $_SERVER['REMOTE_ADDR']                  = (string)($server['remote_addr'] ?? '');
+        $_SERVER['REMOTE_PORT']                  = (string)($server['remote_port'] ?? '');
         $this->getSecureForwardedHeaderParts();
         $this->getCookies();
         $this->getAbsoluteUrl();
-
         $this->getBodyParams();
         $this->setRawBody($this->_request->rawContent() ?: '');
 
@@ -118,7 +114,7 @@ class BaseRequest  extends \yii\web\Request{
 
         $this->getPathInfo();
         $this->resetCounter();
-        Yii::$app->response->clear();
+
 
 
 
