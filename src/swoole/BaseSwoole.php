dfdfd62457c05f0c3897e2ac9aa6f7834bae3ff2
Your Name <you@example.com>
2026-08-02 22:13:51 +0800

    dev-main

--- src/swoole/BaseSwoole.php
+++ src/swoole/BaseSwoole.php
@@ -101,6 +101,7 @@ class BaseSwoole extends BaseObject{
         parent::init();
         if (empty($app=$this->app)) throw new InvalidConfigException('The "app" property must be set.');
         $this->setOption();
+        $this->startQueue();
         if (!$this->server instanceof \Swoole\Http\Server) {
             $this->server = new \Swoole\Http\Server($this->host, $this->port, $this->mode, $this->sockType);
             $this->server->set($this->options);
@@ -214,7 +215,6 @@ class BaseSwoole extends BaseObject{
 
 
     public function onManagerStart(\Swoole\Http\Server $server): void{
-        $this->startQueue();
         $this->log(sprintf('manager started. pid=%d', getmypid()).'info');
     }
 
