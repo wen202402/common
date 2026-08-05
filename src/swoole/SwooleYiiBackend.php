8fb96fc113e3aa1f5b28b2a8db55cb7d7f2de99b
Your Name <you@example.com>
2026-08-05 20:13:09 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -20,7 +20,7 @@ abstract class SwooleYiiBackend extends BaseSwoole{
         parent::onWorkerStart($server, $workerId);
         if ($workerId !== 0) return;
 
-        Timer::tick(1*60 * 1000, function () {$this->actionRibao('order');});
+        Timer::tick(1*60 * 1000, function () {$this->actionRibao();});
         Timer::tick(1 * 1000, function () {$this->actionFund(); });
 
     }
