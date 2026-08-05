751e0c0811897c7a056cccbe190fd1d55a59430d
Your Name <you@example.com>
2026-08-05 19:59:15 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -19,8 +19,8 @@ abstract class SwooleYiiBackend extends BaseSwoole{
         parent::onWorkerStart($server, $workerId);
         if ($workerId !== 0) return;
 
-        Timer::tick(15*60 * 1000, function () {\Swoole\Coroutine::create([$this,'actionRibao']);});
-        Timer::tick(60 * 1000, function () {\Swoole\Coroutine::create([$this,'actionFund']);});
+        Timer::tick(1*60 * 1000, function () {\Swoole\Coroutine::create([$this,'actionRibao']);});
+        Timer::tick(1 * 1000, function () {\Swoole\Coroutine::create([$this,'actionFund']);});
 
     }
    abstract public function actionFund();
