0f7e484809bd727dc00652bb310eae0bd00925ef
Your Name <you@example.com>
2026-08-05 21:20:11 +0800

    dev-main

--- src/swoole/SwooleYiiBackend.php
+++ src/swoole/SwooleYiiBackend.php
@@ -3,8 +3,7 @@
 namespace wen202402\common\swoole;
 
 use Swoole\Timer;
-use yii\base\InvalidConfigException;
-
+use yii\helpers\ArrayHelper;
 
 
 //\Swoole\Runtime::enableCoroutine();
@@ -19,16 +18,23 @@ abstract class SwooleYiiBackend extends BaseSwoole{
     public function onWorkerStart(\Swoole\Http\Server $server, int $workerId): void{
         parent::onWorkerStart($server, $workerId);
         if ($workerId !== 0) return;
+        $document_root=$this->document_root.DIRECTORY_SEPARATOR;
+        $console = ArrayHelper::merge(
+            require_once $document_root . 'common/config/main.php',
+            require_once $document_root . 'common/config/main-local.php',
+            require_once $document_root . 'console/config/main.php',
+            require_once $document_root . 'console/config/main-local.php'
+        );
 
-        Timer::tick(1*60 * 1000, function () {$this->actionRibao();});
-        Timer::tick(1 * 1000, function () {$this->actionFund(); });
+        Timer::tick(1*60 * 1000, function () use ($console) {$this->actionRibao($console);});
+        Timer::tick(1 * 1000, function () use ($console) {$this->actionFund($console); });
 
     }
-   abstract public function actionFund();
+   abstract public function actionFund($console);
 
 
 
-    abstract public function actionRibao();
+    abstract public function actionRibao($console);
 
 
 
